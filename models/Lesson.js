be87016d2dd6ea01a9732f89520bbcb7146a5b45
ErenBerkSari <erenmxc@gmail.com>
2024-10-16 16:18:49 +0300

    associated with the category model

--- models/Lesson.js
+++ models/Lesson.js
@@ -25,6 +25,10 @@ const LessonSchema = new mongoose.Schema({
     type: Date,
     default: new Date(),
   },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
 });
 
 const Lesson = mongoose.model("Lesson", LessonSchema);
