0c32b1430134c575022aae15dca26a5db3ac84fb
ErenBerkSari <erenmxc@gmail.com>
2024-12-20 00:54:59 +0300

    middleware updated

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,33 +3,35 @@ const app = express();
 app.use(cookieParser());
 
 const authMiddleware = (req, res, next) => {
-  const token = req.cookies.accessToken; // Cookie'den token al
-  console.log("tokeni neden alamıyorum:", token);
-  console.log("Headers:", req.headers);
-  console.log("Cookies:", req.cookies);
+  // const token = req.cookies.accessToken; // Cookie'den token al
+  // console.log("tokeni neden alamıyorum:", token);
+  // console.log("Headers:", req.headers);
+  // console.log("Cookies:", req.cookies);
 
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized, token missing" });
-  }
+  // if (!token) {
+  //   return res.status(401).json({ message: "Unauthorized, token missing" });
+  // }
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      if (err.name === "TokenExpiredError") {
-        return res.status(403).json({ message: "Token has expired" });
-      } else {
-        return res.status(403).json({ message: "Invalid token" });
-      }
-    }
+  // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  //   if (err) {
+  //     if (err.name === "TokenExpiredError") {
+  //       return res.status(403).json({ message: "Token has expired" });
+  //     } else {
+  //       return res.status(403).json({ message: "Invalid token" });
+  //     }
+  //   }
 
-    // Doğrulanan kullanıcı bilgilerini req.user'a ekle
-    req.user = {
-      userId: decoded.userId,
-      role: decoded.role,
-    };
+  //   // Doğrulanan kullanıcı bilgilerini req.user'a ekle
+  //   req.user = {
+  //     userId: decoded.userId,
+  //     role: decoded.role,
+  //   };
 
-    console.log("Authenticated User:", req.user);
-    next();
-  });
+  //   console.log("Authenticated User:", req.user);
+  //   next();
+  // });
+  console.log("Middleware'deki cookies:", req.cookies);
+  next();
 };
 
 module.exports = authMiddleware;
