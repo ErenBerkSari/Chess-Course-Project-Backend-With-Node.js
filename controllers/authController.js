b4e094c571e7a2c818e4f845975df34b3858815d
ErenBerkSari <erenmxc@gmail.com>
2025-01-03 17:00:14 +0300

    authController updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -126,7 +126,7 @@ const login = async (req, res) => {
     const accessToken = jwt.sign(
       { userId: user._id, role: user.role },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "30m" }
+      { expiresIn: "1m" }
     );
     const refreshToken = jwt.sign(
       { userId: user._id },
